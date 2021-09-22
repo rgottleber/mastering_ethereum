4a083a6aa7955763c57aba3ae7d05b55d14d24ba
Richard Gottleber <richard@gottleber.net>
2021-09-22 18:31:18 -0500

    add deploy setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+ require('dotenv').config();
+ require("@nomiclabs/hardhat-waffle");
  module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.RINKEBY_PRIVATE_KEY}`],
+    },
+  },
 };
